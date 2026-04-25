403eab7e0fd28fada90fb036c2929d8a9bd4eec2
Nwuzor Christian <167423382+Metu123@users.noreply.github.com>
2026-04-25 07:57:00 +0100

    Correct email verification message capitalization

--- realestate/resources/views/verify-email.blade.php
+++ realestate/resources/views/verify-email.blade.php
@@ -15,7 +15,7 @@
             <p>Your email is <b>not verified</b>. You can still log in, but verification is recommended.</p>
             <a href="{{ url('/send-verification-email') }}" class="btn btn-success"> <i class="fas fa-paper-plane"></i> Resend Verification Email</a>
         @else
-            <p>Your email is <b>verified</b>. Enjoy full access to all features!</p>
+            <p>Your email is <b>verified</b>. you have full access to all features!</p>
             <a href="{{ url('/') }}" class="btn btn-sucess"> <i class="fas fa-home"></i> Go to Homepage</a>
         @endif
     </div>
